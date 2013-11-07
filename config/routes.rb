4b4815e027ea8f64bfad7c006c3e8f2e001e6fc1
Jonathan <jarp@nd.edu>
2013-11-06 19:37:55 -0500

    routes hell

--- config/routes.rb
+++ config/routes.rb
@@ -5,17 +5,11 @@ ChoreBoardApp::Application.routes.draw do
   get "home/index", :as => :home
   get "home/about", :as => :about
 
-  get ":family_id/todos/list" => 'todos#list', :as => :family_todos_list
-  
-
   resources :todos
   resources :tasks
   resources :intervals
   resources :categories
 
-  get '/login' => 'login#index', :as => :login
-  post '/login' => 'login#authenticate', :as => :process_login
-  get '/logout' => 'login#logout', :as => :logout
 
   #match ':family_id/:controller/(:action)/(:id)' 
 
@@ -24,17 +18,20 @@ ChoreBoardApp::Application.routes.draw do
   resources :member_types
   resources :families
 
-    resources :families, path: "/" do 
-       resources :members
-       resources :tasks
-       resources :todos
-    end
-  
+  resources :families, path: "/" do 
+     resources :members
+     resources :tasks
+     resources :todos
+  end
+
+  get ":family_id/todos/list" => 'todos#list', :as => :family_todos_list
+  get '/login' => 'login#index', :as => :login
+  post '/login' => 'login#authenticate', :as => :process_login
+  get '/logout' => 'login#logout', :as => :logout
   match '/todos/:id/assign/:member_id' => 'todos#assign', :as => :assign_todo
   match '/todos/:id/unassign/:member_id' => 'todos#unassign', :as => :unassign_todo
 
     match '/todos/:id/complete/:member_id' => 'todos#complete', :as => :complete_todo
   match '/todos/:id/incomplete/:member_id' => 'todos#incomplete', :as => :incomplete_todo
-
 
 end
