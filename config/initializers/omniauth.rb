25a70b6a0d04cbfb10159959adb780d383d05fd3
Jonathan <jarp@nd.edu>
2013-11-07 14:09:14 -0500

    added init

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,7 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, ENV["GOOGLE_KEY"], ENV["GOOGLE_SECRET"],{
+  	:name => 'google',
+  	:scope => 'userinfo.email'
+
+  }
+end
